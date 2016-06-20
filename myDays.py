c9d9ed523af4a190c985004def836f9c21e4c20e
/sable <sable@cantus.us>
2016-06-20 16:08:52 -0700

    count days project

---
+++ myDays.py
@@ -0,0 +1,18 @@
+# WASTC 2016 Exercises
+# Sable Cantus
+
+from datetime import date
+
+start_date = date(1970, 1, 1)
+end_date = date(2016, 6, 20)
+delta = end_date - start_date
+
+#print('The number of days from ', start_date, 'to', end_date, 'is ', delta)
+
+print('Start Date: ', start_date)
+print('End Date: ', end_date)
+print('Days: ',delta)
+
+
+
+
