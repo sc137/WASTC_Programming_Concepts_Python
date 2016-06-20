cff1747c71ce607f75dc1c0fcbc7fb4c214eed15
sable <sable@Drogon.local>
2016-06-20 13:50:04 -0700

    getting shared notes

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
 print ('Hello, world!')
+
+a = 1
+b = 100
+c = a + b
+
+##if a == 1:
+##    print ('a is 1')
+##
+##else:
+##    print ('a is not 1')
+
+print ('a =', a)
+print ('b =', b)
+print ('c =', c)
