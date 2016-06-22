2bed7234ad9300d5413165f7ebca3507cbef20bf
Dana Arazi <sales@dansid.com>
2016-06-22 13:08:34 -0700

    update day three after lunch

---
+++ Day3Class/testing.py
@@ -0,0 +1,9 @@
+# copy and paste this into the python comand line
+
+for x in range(0,3):
+    print("we are on time %d" % x)
+print(x)
+
+for i in range (4):
+    for j in range(4):
+        print("i=%d j=%d i+j=%d" % (i, j, i+j))
